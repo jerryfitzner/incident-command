fe31654db408ffdd7c9643bec39f304e06694314
Jerry Fitzner <jfitzner1@gmail.com>
2023-02-01 13:53:16 -0500

    Corrected Emergency Vehicle Validations

--- app/models/emergency_vehicle.rb
+++ app/models/emergency_vehicle.rb
@@ -1,11 +1,11 @@
 class EmergencyVehicle < ApplicationRecord
   belongs_to :agency
-  belongs_to :incident
+  belongs_to :incident, optional: true 
 
   validates :status, presence: true, inclusion: { in: %w(Enroute Arrived Assigned Available Unassigned), message: "%{value} is not a valid response status" }
   validates :crew_size, numericality: { only_integer: true }
-  validates :active, presence: true, inclusion: [true, false]
+  validates :active, inclusion: [true, false]
   validates :call_sign, presence: true, length: { in: 2..20 }
   validates_associated :agency
-  validates :incident_id, numericality: { only_integer: true }
+  validates :incident_id, numericality: { only_integer: true, allow_nil: true }
 end
