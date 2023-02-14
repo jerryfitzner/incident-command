dac204b764d18f5e56b76eadb23faaa98a3d1065
Jerry Fitzner <jfitzner1@gmail.com>
2023-02-14 16:01:31 -0500

    Updated Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
   skip_before_action :must_login, only: [ :create ]
 
   def create
-    binding.pry
-    user = user.find_by(username: params[:username])
+    # binding.pry
+    user = User.find_by(username: params[:username])
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       render json: user, status: :created
     else 
-      render json: { error: "Invalid username or password." }, status: :unathorized
+      render json: { error: "Invalid username or password." }, status: :unauthorized
     end
   end
 
