8e3e8f47cfdd490952d372bfc9789be950cbc929
Jerry Fitzner <94945473+jerryfitzner@users.noreply.github.com>
2023-02-27 17:59:19 -0500

    Changed Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  namespace :api do
-    resources :agencies
-    resources :emergency_vehicles
-    resources :addresses
-    resources :incidents
-    resources :users
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  resources :agencies
+  resources :emergency_vehicles
+  resources :addresses
+  resources :incidents
+  resources :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-    get '/me', to: 'users#show'
-    get '/agency-names', to: 'agencies#only_agency_names'
+  get '/me', to: 'users#show'
+  get '/agency-names', to: 'agencies#only_agency_names'
 
-    post '/login', to: 'sessions#create'
+  post '/login', to: 'sessions#create'
 
-    delete '/logout', to: 'sessions#destroy'
-  end
+  delete '/logout', to: 'sessions#destroy'
+  
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   
