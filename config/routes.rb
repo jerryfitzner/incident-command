ca9ae7959a69ae0b59dda78d00e6b9471515b336
Jerry Fitzner <jfitzner1@gmail.com>
2023-02-27 14:07:23 -0500

    Updated for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
-  resources :agencies
-  resources :emergency_vehicles
-  resources :addresses
-  resources :incidents
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :agencies
+    resources :emergency_vehicles
+    resources :addresses
+    resources :incidents
+    resources :users
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    get '/me', to: 'users#show'
+    get '/agency-names', to: 'agencies#only_agency_names'
 
-  get '/me', to: 'users#show'
-  get '/agency-names', to: 'agencies#only_agency_names'
+    post '/login', to: 'sessions#create'
 
-  post '/login', to: 'sessions#create'
-  # post '/evupdate', to 'emergency_vehicles#update_return_incident'
+    delete '/logout', to: 'sessions#destroy'
+  end
 
-  delete '/logout', to: 'sessions#destroy'
-
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   
 end
